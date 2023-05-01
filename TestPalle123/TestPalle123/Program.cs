c4e2bc0d799f576dbd143d3c349ad9d3f162a690
SimonO2001 <98746432+SimonO2001@users.noreply.github.com>
2023-05-01 09:29:11 +0200

    Add files via upload

---
+++ TestPalle123/TestPalle123/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace TestPalle123 
+{
+    public class Program 
+    {
+        static void Main(string[] args)
+        {
+            bool loop = true;
+
+            while (loop) 
+            {
+                Console.WriteLine("Hvor hurtigt løber du");
+
+                int hastighed = Convert.ToInt32(Console.ReadLine());
+
+                if (hastighed < 9 && hastighed > 0)
+                {
+                    Console.WriteLine("Du er med i VM!");
+                }
+                else if (hastighed < 10 && hastighed > 0)
+                {
+                    Console.WriteLine("Du er med i EM!");
+                }
+                else if (hastighed >= 11 && hastighed > 0)
+                {
+                    Console.WriteLine("Du er med i EJ!");
+                }
+                else if (hastighed < 12 && hastighed > 0)
+                {
+                    Console.WriteLine("Du er med i INGENTING!");
+                }
+                else if (hastighed == 0)
+                {
+                    Console.WriteLine("Du er ude af loopen nu!");
+
+                    loop = false;
+                }
+
+                Console.ReadKey();
+
+
+            }
+            
+
+            
+
+        }
+
+    }
+    
+
+
+}
+
